43e46998fbbdfa87d29f1f263102f1d55450afab
rukshan <ruky68@live.com>
2015-06-03 14:51:29 +0530

    adding health module

--- services/geowave-service-stats/src/main/java/mil/nga/giat/geowave/service/stats/Client.java
+++ services/geowave-service-stats/src/main/java/mil/nga/giat/geowave/service/stats/Client.java
@@ -1,25 +1,10 @@
 package mil.nga.giat.geowave.service.stats;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.TreeSet;
-
-import mil.nga.giat.geowave.datastore.accumulo.AccumuloRowId;
-import mil.nga.giat.geowave.datastore.accumulo.BasicAccumuloOperations;
-import mil.nga.giat.geowave.datastore.accumulo.util.AccumuloUtils;
-
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.BatchWriter;
-import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Instance;
-import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.ZooKeeperInstance;
-import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
-import org.apache.accumulo.core.security.ColumnVisibility;
 import org.apache.hadoop.io.Text;
 
 public class Client {
@@ -31,86 +16,53 @@ public class Client {
 		Instance inst = new ZooKeeperInstance(instanceName, zooServers);
 
 		try {
-			Connector conn = inst.getConnector("root", "password");
-			System.out.println(conn.whoami());
+			// Connector conn = inst.getConnector("root", "password");
+			// System.out.println(conn.whoami());
 
-			TableOperations t = conn.tableOperations();
-			// t.create("ruks");
+			Text t = new Text("rukshakn12");
 
-			TreeSet<Text> splits = new TreeSet<Text>();
-			for (int i = 0; i < 256; i++) {
-				byte[] bytes = { (byte) i };
-				splits.add(new Text(bytes));
-			}
+			Text a = new Text("aa");
+			Text b = new Text("bb");
+			Text c = new Text("cc");
+			Text d = new Text("dd");
 
-			// t.addSplits("ruks", splits);
+			byte[] bb = new byte[0];
 
-			Collection<Text> c = t.listSplits("ruks");
-			ArrayList<Text> txt = new ArrayList<Text>(c);
-			System.out.println(c.size());
-			System.out.println(txt.get(0));
+			Key k = new Key(a.getBytes(), 0, a.getLength(), c.getBytes(), 0,
+					b.getLength(), c.getBytes(), 0, c.getLength(),
+					d.getBytes(), 0, d.getLength(), Long.MAX_VALUE);
 
-			Key key = new Key(txt.get(0));
-			AccumuloRowId id = new AccumuloRowId(key);
+			// AccumuloRowIds ids = new AccumuloRowIds(k);
 
-		} catch (AccumuloException | AccumuloSecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TableNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Mutation m = new Mutation(new Text(String.format("row_%d", 1)));
+			Text t1 = new Text(String.format("colqual_%d", 0));
+			Value v = new Value((String.format("value_%d_%d", 0, 1)).getBytes());
+			Text colf = new Text("colfam");
+
+			m.put(colf, t1, v);
+
+			AccumuloRowIds id = new AccumuloRowIds(m.getRow());
+
+			// TieredSFCIndexStrategy tt=new
+			// TieredSFCIndexStrategy(baseDefinitions, orderedSfcs,
+			// orderedSfcIndexToTierId);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		// ListInstances li=new ListInstances();
-
-		// AccumuloRowId id = new AccumuloRowId(key);
-		// System.out.println(id.getInsertionId());
 	}
 
-	public static void addData() {
+	public static void main(String[] arsg) {
+		// String instanceName = "geowave";
+		// String zooServers = "127.0.0.1";
+		// Instance inst = new ZooKeeperInstance(instanceName, zooServers);
+		//
 
-	}
-
-	public static void createRuks(Connector conn) throws Exception {
-		Text rowID = new Text("row1");
-		Text colFam = new Text("myColFam");
-		Text colQual = new Text("myColQual");
-		ColumnVisibility colVis = new ColumnVisibility("public");
-		long timestamp = System.currentTimeMillis();
-
-		Value value = new Value("myValue".getBytes());
-
-		Mutation mutation = new Mutation(rowID);
-		mutation.put(colFam, colQual, colVis, timestamp, value);
-
-		long memBuf = 1000000L; // bytes to store before sending a batch
-		long timeout = 1000L; // milliseconds to wait before sending
-		int numThreads = 10;
-
-		BatchWriter writer = conn.createBatchWriter("table", memBuf, timeout,
-				numThreads);
-
-		writer.addMutation(mutation);
-
-		writer.close();
-
-	}
-
-	public static void main(String[] args) throws Exception {
-		String instanceName = "geowave";
-		String zooServers = "127.0.0.1";
-		Instance inst = new ZooKeeperInstance(instanceName, zooServers);
-
-		Connector conn = inst.getConnector("root", "password");
-		AccumuloUtils au = new AccumuloUtils();
-
-		BasicAccumuloOperations ba = new BasicAccumuloOperations(conn);
-		ba.createTable("ruks1");
-		// Index i=new
-		// au.setSplitsByNumSplits(conn, "ruks", index, 5);
+//		TieredSFCIndexStrategy tt = new TieredSFCIndexStrategy(baseDefinitions, orderedSfcs, orderedSfcIndexToTierId);
+//
+//		ByteArrayId ids = new ByteArrayId("qsdfdsfwew");
+//		tt.getCoordinatesPerDimension(ids);
 
 	}
 }
