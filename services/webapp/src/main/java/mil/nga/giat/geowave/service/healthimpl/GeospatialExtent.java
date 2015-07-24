8d1357ec22ece900c9801e777e6cd753cb3d3f15
rukshan <ruky68@live.com>
2015-07-25 01:21:26 +0530

    decodedRows

--- services/webapp/src/main/java/mil/nga/giat/geowave/service/healthimpl/GeospatialExtent.java
+++ services/webapp/src/main/java/mil/nga/giat/geowave/service/healthimpl/GeospatialExtent.java
@@ -1,10 +1,24 @@
 package mil.nga.giat.geowave.service.healthimpl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.SortedSet;
 import java.util.TreeSet;
+
+import mil.nga.giat.geowave.accumulo.AccumuloOperations;
+import mil.nga.giat.geowave.accumulo.BasicAccumuloOperations;
+import mil.nga.giat.geowave.accumulo.metadata.AccumuloAdapterStore;
+import mil.nga.giat.geowave.accumulo.util.AccumuloUtils;
+import mil.nga.giat.geowave.datastore.accumulo.AccumuloRowId;
+//import mil.nga.giat.geowave.core.geotime.IndexType;
+import mil.nga.giat.geowave.index.ByteArrayId;
+import mil.nga.giat.geowave.store.adapter.DataAdapter;
+import mil.nga.giat.geowave.store.index.Index;
+import mil.nga.giat.geowave.store.index.IndexType;
+//import mil.nga.giat.geowave.adapter.vector.FeatureDataAdapter;
+import mil.nga.giat.geowave.vector.adapter.FeatureDataAdapter;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
@@ -29,39 +43,31 @@ import org.apache.accumulo.core.data.impl.KeyExtent;
 import org.apache.accumulo.core.iterators.user.WholeRowIterator;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.hadoop.io.Text;
+import org.geotools.feature.AttributeTypeBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.geometry.Geometry;
 
-public class GeospatialExtent
-{
+public class GeospatialExtent {
 
-	public static void main(
-			String[] args )
-			throws Exception {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		String instanceName = "geowave";
 		String zooServers = "127.0.0.1";
-		Instance inst = new ZooKeeperInstance(
-				instanceName,
-				zooServers);
+		Instance inst = new ZooKeeperInstance(instanceName, zooServers);
 		Connector conn;
-		AuthenticationToken authToken = new PasswordToken(
-				"password");
-		conn = inst.getConnector(
-				"root",
-				authToken);
-		addSplits(conn);
-		// getSplits(conn, inst);
+		AuthenticationToken authToken = new PasswordToken("password");
+		conn = inst.getConnector("root", authToken);
+		// addSplits(conn);
+		getSplits(conn, inst);
 	}
 
-	public static void addSplits(
-			Connector conn ) {
+	public static void addSplits(Connector conn) {
 		Authorizations auths = new Authorizations();
 		Scanner scan;
 		try {
-			scan = conn.createScanner(
-					"ruks_SPATIAL_VECTOR_IDX",
-					auths);
-		}
-		catch (TableNotFoundException e1) {
+			scan = conn.createScanner("ruks_SPATIAL_VECTOR_IDX", auths);
+		} catch (TableNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			return;
@@ -77,136 +83,91 @@ public class GeospatialExtent
 			Key k = entry.getKey();
 			if (cnt == 300) {
 				keys.add(k.getRow());
-			}
-			else if (cnt == 600) {
+			} else if (cnt == 600) {
 				keys.add(k.getRow());
-			}
-			else if (cnt == 900) {
+			} else if (cnt == 900) {
 				keys.add(k.getRow());
 			}
 			cnt++;
 		}
 
 		try {
-			op.addSplits(
-					"ruks_SPATIAL_VECTOR_IDX",
-					keys);
-		}
-		catch (TableNotFoundException | AccumuloException | AccumuloSecurityException e) {
+			op.addSplits("ruks_SPATIAL_VECTOR_IDX", keys);
+		} catch (TableNotFoundException | AccumuloException
+				| AccumuloSecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static void getSplits(
-			Connector conn,
-			Instance inst ) {
+	public static void getSplits(Connector conn, Instance inst) {
 		TableOperations op = conn.tableOperations();
 		System.out.println(op.tableIdMap());
 		String table = "ruks_SPATIAL_VECTOR_IDX";
-		String tid = op.tableIdMap().get(
-				"ruks_SPATIAL_VECTOR_IDX");
+		String tid = op.tableIdMap().get("ruks_SPATIAL_VECTOR_IDX");
 
 		try {
-			List<Text> list = new ArrayList<Text>(
-					op.listSplits(table));
+			List<Text> list = new ArrayList<Text>(op.listSplits(table));
 			System.out.println(list.size());
 
 			ClientConfiguration clientConf = ClientConfiguration.loadDefault();
 
 			Instance accInstance = inst;
-			ClientContext ctx = new ClientContext(
-					accInstance,
-					new Credentials(
-							"root",
-							new PasswordToken(
-									"password")),
-					clientConf);
-			TabletLocator tl = TabletLocator.getLocator(
-					ctx,
-					new Text(
-							tid));
+			ClientContext ctx = new ClientContext(accInstance, new Credentials(
+					"root", new PasswordToken("password")), clientConf);
+			TabletLocator tl = TabletLocator.getLocator(ctx, new Text(tid));
 			System.out.println();
 
 			TabletLocation tt;
 			String loc;
+			ArrayList<Range> ranges = new ArrayList<Range>();
 			Range r;
 			String uuid;
 			KeyExtent ke;
 			for (int i = 0; i < list.size(); i++) {
-				tt = tl.locateTablet(
-						ctx,
-						list.get(i),
-						false,
-						false);
+				tt = tl.locateTablet(ctx, list.get(i), false, false);
 				System.out.println(tt.tablet_location);
 				ke = tt.tablet_extent;
-				loc = tl.locateTablet(
-						ctx,
-						ke.getEndRow(),
-						false,
-						false).tablet_location;
+				loc = tl.locateTablet(ctx, ke.getEndRow(), false, false).tablet_location;
 				System.out.println(loc);
-				// r = new Range(
-				// list.get(i),
-				// ke.getEndRow());
+				r = new Range(list.get(i), ke.getEndRow());
+				ranges.add(r);
 				uuid = ke.getUUID().toString();
 				System.out.println(uuid);
 			}
 
 			Text first, last;
-			Key[] kk = read(
-					list.get(list.size() - 1),
-					conn,
-					table);
+			Key[] kk = read(list.get(list.size() - 1), conn, table);
 			first = kk[0].getRow();
 			last = kk[1].getRow();
 
-			tt = tl.locateTablet(
-					ctx,
-					first,
-					false,
-					false);
+			tt = tl.locateTablet(ctx, first, false, false);
 			System.out.println(tt.tablet_location);
 			ke = tt.tablet_extent;
-			loc = tl.locateTablet(
-					ctx,
-					last,
-					false,
-					false).tablet_location;
+			loc = tl.locateTablet(ctx, last, false, false).tablet_location;
 			System.out.println(loc);
-			// r = new Range(
-			// first,
-			// last);
+			r = new Range(first, last);
+			ranges.add(r);
 			System.out.println(ke.getUUID());
 
-		}
-		catch (AccumuloException | TableNotFoundException e1) {
+			extent(conn, table, ranges.get(ranges.size() - 1), accInstance);
+		} catch (AccumuloException | TableNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (AccumuloSecurityException e) {
+		} catch (AccumuloSecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static Key[] read(
-			Text end,
-			Connector conn,
-			String table ) {
+	public static Key[] read(Text end, Connector conn, String table) {
 
 		try {
 
 			Authorizations auths = new Authorizations();
-			Scanner scan = conn.createScanner(
-					table,
-					auths);
-			scan.setRange(new Range(
-					end,
-					null));
-			IteratorSetting itSettings = new IteratorSetting(
-					1,
+			Scanner scan = conn.createScanner(table, auths);
+			scan.setRange(new Range(end, null));
+			IteratorSetting itSettings = new IteratorSetting(1,
 					WholeRowIterator.class);
 			scan.addScanIterator(itSettings);
 
@@ -220,16 +181,74 @@ public class GeospatialExtent
 				}
 			}
 			last = k;
-			return new Key[] {
-				first,
-				last
-			};
+			return new Key[] { first, last };
 
-		}
-		catch (TableNotFoundException e) { // TODO Auto-generated catch block
+		} catch (TableNotFoundException e) { // TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 
+	}
+
+	public static void extent(Connector conn, String table, Range range,
+			Instance inst) {
+
+		Index index = IndexType.SPATIAL_VECTOR.createDefaultIndex();
+
+		try {
+
+			Authorizations auths = new Authorizations();
+			Scanner scan = conn.createScanner(table, auths);
+			scan.setRange(range);
+			IteratorSetting itSettings = new IteratorSetting(1,
+					WholeRowIterator.class);
+			scan.addScanIterator(itSettings);
+
+			FeatureDataAdapter s = null;
+			for (Entry<Key, Value> entry : scan) {
+				AccumuloRowId id = new AccumuloRowId(entry.getKey());
+				ByteArrayId bid = new ByteArrayId(id.getAdapterId());
+
+				AccumuloOperations ao = new BasicAccumuloOperations(conn);
+				AccumuloAdapterStore a = new AccumuloAdapterStore(ao);
+				DataAdapter<?> adapter = a.getAdapter(bid);
+
+				Object o=AccumuloUtils.decodeRow(entry.getKey(), entry.getValue(),
+						adapter, index);
+				System.out.println(o);
+			}
+
+		} catch (TableNotFoundException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	protected static SimpleFeatureType createPointFeatureType() {
+
+		final SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+		final AttributeTypeBuilder ab = new AttributeTypeBuilder();
+
+		builder.setName("Point");
+
+		builder.add(ab.binding(Geometry.class).nillable(false)
+				.buildDescriptor("geometry"));
+		builder.add(ab.binding(Date.class).nillable(true)
+				.buildDescriptor("TimeStamp"));
+		builder.add(ab.binding(Double.class).nillable(false)
+				.buildDescriptor("Latitude"));
+		builder.add(ab.binding(Double.class).nillable(false)
+				.buildDescriptor("Longitude"));
+		builder.add(ab.binding(String.class).nillable(true)
+				.buildDescriptor("TrajectoryID"));
+		builder.add(ab.binding(String.class).nillable(true)
+				.buildDescriptor("Comment"));
+
+		return builder.buildFeatureType();
+	}
+
+	public static FeatureDataAdapter createDataAdapter(
+			final SimpleFeatureType sft) {
+		return new FeatureDataAdapter(sft);
 	}
 }
--- services/webapp/src/main/java/mil/nga/giat/geowave/service/healthimpl/Test.java
+++ services/webapp/src/main/java/mil/nga/giat/geowave/service/healthimpl/Test.java
@@ -1,207 +1,73 @@
 package mil.nga.giat.geowave.service.healthimpl;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Map.Entry;
 
-import mil.nga.giat.geowave.service.jaxbbean.TabletBean;
-
-import org.apache.accumulo.core.client.ClientConfiguration;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.ZooKeeperInstance;
-import org.apache.accumulo.core.client.impl.ClientContext;
-import org.apache.accumulo.core.client.impl.Credentials;
-import org.apache.accumulo.core.client.impl.MasterClient;
-import org.apache.accumulo.core.client.impl.thrift.ThriftSecurityException;
+import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.client.security.tokens.AuthenticationToken;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
-import org.apache.accumulo.core.data.impl.KeyExtent;
-import org.apache.accumulo.core.master.thrift.MasterClientService;
-import org.apache.accumulo.core.master.thrift.MasterMonitorInfo;
-import org.apache.accumulo.core.rpc.ThriftUtil;
-import org.apache.accumulo.core.tabletserver.thrift.TabletClientService;
-import org.apache.accumulo.core.tabletserver.thrift.TabletStats;
-import org.apache.accumulo.core.trace.Tracer;
-import org.apache.accumulo.core.util.Base64;
-import org.apache.accumulo.server.AccumuloServerContext;
-import org.apache.accumulo.server.conf.ServerConfigurationFactory;
-import org.apache.thrift.TException;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.security.Authorizations;
+import org.apache.hadoop.io.Text;
 
-import com.google.common.net.HostAndPort;
+public class Test {
 
-public class Test
-{
-
-	MasterMonitorInfo inf = null;
-	AccumuloServerContext context;
-	HostAndPort address;
-	MasterClientService.Iface c;
-	ClientContext ctx;
-
-	public Test() {
-
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
 		String instanceName = "geowave";
 		String zooServers = "127.0.0.1";
-		Instance inst = new ZooKeeperInstance(
-				instanceName,
-				zooServers);
-		Instance accInstance = inst;
-		ClientConfiguration clientConf = ClientConfiguration.loadDefault();
-		ctx = new ClientContext(
-				accInstance,
-				new Credentials(
-						"root",
-						new PasswordToken(
-								"password")),
-				clientConf);
-
-		ServerConfigurationFactory config;
-
-		config = new ServerConfigurationFactory(
-				inst);
-		context = new AccumuloServerContext(
-				config);
-
-		c = MasterClient.getConnectionWithRetry(context);
-
+		Instance inst = new ZooKeeperInstance(instanceName, zooServers);
+		Connector conn;
+		AuthenticationToken authToken = new PasswordToken("password");
+		conn = inst.getConnector("root", authToken);
+		addSplits(conn);
 	}
 
-	private static double stddev(
-			double elapsed,
-			double num,
-			double sumDev ) {
-		if (num != 0) {
-			double average = elapsed / num;
-			return Math.sqrt((sumDev / num) - (average * average));
-		}
-		return 0;
-	}
-
-	public List<TabletBean> getTabletStats(
-			String tid,
-			String tserver )
-			throws TException,
-			NoSuchAlgorithmException {
-
-		String table;
-		String tablet;
-		long entries;
-		double ingest;
-		double query;
-		double miAvg = 0;
-		double mistd = 0;
-		double miAvges = 0;
-		double maAvg = 0;
-		double mastd = 0;
-		double maAvges = 0;
-
-		address = HostAndPort.fromString(tserver);
-		List<TabletBean> stat = new ArrayList<TabletBean>();
-
+	public static void addSplits(Connector conn) {
+		Authorizations auths = new Authorizations();
+		Scanner scan;
 		try {
-			inf = c.getMasterStats(
-					Tracer.traceInfo(),
-					context.rpcCreds());
-		}
-		catch (TException e1) {
+			scan = conn.createScanner("ruks_SPATIAL_VECTOR_IDX", auths);
+		} catch (TableNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
+			return;
+		}
+
+		System.out.println("size " + scan.getBatchSize());
+
+		TableOperations op = conn.tableOperations();
+
+		int cnt = 0;
+		SortedSet<Text> keys = new TreeSet<Text>();
+		for (Entry<Key, Value> entry : scan) {
+			Key k = entry.getKey();
+			if (cnt == 300) {
+				keys.add(k.getRow());
+			} else if (cnt == 600) {
+				keys.add(k.getRow());
+			} else if (cnt == 900) {
+				keys.add(k.getRow());
+			}
+			cnt++;
 		}
 
 		try {
-			TabletClientService.Client client = ThriftUtil.getClient(
-					new TabletClientService.Client.Factory(),
-					address,
-					ctx);
-			List<TabletStats> li = client.getTabletStats(
-					Tracer.traceInfo(),
-					context.rpcCreds(),
-					tid);
-			System.out.println(li.size());
-			System.out.println(li);
-
-			List<TabletStats> tsStats = new ArrayList<TabletStats>();
-
-			for (String tableId : inf.tableMap.keySet()) {
-				tsStats.addAll(client.getTabletStats(
-						Tracer.traceInfo(),
-						context.rpcCreds(),
-						tableId));
-			}
-
-			for (TabletStats info : tsStats) {
-
-				if (info.extent == null) {
-					continue;
-				}
-
-				KeyExtent extent = new KeyExtent(
-						info.extent);
-				String tableId = extent.getTableId().toString();
-
-				MessageDigest digester = MessageDigest.getInstance("MD5");
-				if (extent.getEndRow() != null && extent.getEndRow().getLength() > 0) {
-					digester.update(
-							extent.getEndRow().getBytes(),
-							0,
-							extent.getEndRow().getLength());
-				}
-				String obscuredExtent = Base64.encodeBase64String(digester.digest());
-
-				table = tableId;
-				tablet = obscuredExtent;
-
-				entries = info.numEntries;
-				ingest = info.ingestRate;
-				query = info.queryRate;
-
-				miAvg = info.minors.num != 0 ? info.minors.elapsed / info.minors.num : 0;
-				mistd = stddev(
-						info.minors.elapsed,
-						info.minors.num,
-						info.minors.sumDev);
-				miAvges = info.minors.elapsed != 0 ? info.minors.count / info.minors.elapsed : 0;
-				maAvg = info.majors.num != 0 ? info.majors.elapsed / info.majors.num : 0;
-				mastd = stddev(
-						info.majors.elapsed,
-						info.majors.num,
-						info.majors.sumDev);
-				maAvges = info.majors.elapsed != 0 ? info.majors.count / info.majors.elapsed : 0;
-				stat.add(new TabletBean(
-						table,
-						tablet,
-						entries,
-						ingest,
-						query,
-						miAvg,
-						mistd,
-						miAvges,
-						maAvg,
-						mastd,
-						maAvges));
-			}
-
-		}
-		catch (ThriftSecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-
-		return stat;
-	}
-
-	public static void main(
-			String[] args ) {
-		Test t = new Test();
-		try {
-			System.out.println(t.getTabletStats(
-					"2",
-					"rukshan-ThinkPad-T540p:50964").size());
-		}
-		catch (NoSuchAlgorithmException | TException e) {
+			op.addSplits("ruks_SPATIAL_VECTOR_IDX", keys);
+		} catch (TableNotFoundException | AccumuloException
+				| AccumuloSecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 }
